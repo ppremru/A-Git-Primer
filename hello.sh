a0da23830be7c2effad6dc4d73766c0f27d363a1
Peggy Premru <ppremru@redhat.com>
2026-03-25 01:43:32 -0400

    snapshot for four

--- hello.sh
+++ hello.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo Hello World, life is good
-echo Publish is not a git option, it is vscode term ... 
+echo Publish is not a git option, it is GitDash term
+echo Sync is not a git option, it is a GitDash term
+
+echo this is in four but not in main
