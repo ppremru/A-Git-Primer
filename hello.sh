6c6daf431cea82ebda8c86db16a91246a78ed592
Peggy <57100258+ppremru@users.noreply.github.com>
2026-03-25 01:49:05 -0400

    Four (#5)
    * snapshot for four
    
    * removed a line

--- hello.sh
+++ hello.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo Hello World, life is good
-echo Publish is not a git option, it is vscode term ... 
+echo Publish is not a git option, it is GitDash term
+echo Sync is not a git option, it is a GitDash term
